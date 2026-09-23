d9bae770db89900d257296546e3f71928d8e3ac7
maneesoam <maneesoam007@gmail.com>
2026-09-23 11:17:01 +0530

    Time: 0 ms (100%), Space: 9.6 MB (73.83%) - LeetHub

--- 3498-reverse-degree-of-a-string/3498-reverse-degree-of-a-string.cpp
+++ 3498-reverse-degree-of-a-string/3498-reverse-degree-of-a-string.cpp
@@ -1,14 +1,13 @@
 class Solution {
 public:
     int reverseDegree(string s) {
-     int n =s.size();
-     int ans = 0;
+     int n = s.size();
+     int ans =0;
      for(int i=0;i<n;i++){
-        int reverseValue ='z' -s[i]+1;
-        ans+=reverseValue *(i+1);
-
- 
-     } 
-     return ans;  
+        int value='z'-s[i]+1;
+        ans+=value*(i+1);
+     }
+     return ans;
+      
     }
 };
